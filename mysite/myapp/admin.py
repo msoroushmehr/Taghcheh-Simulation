712762839324b77829c57ef991dc4f3a1c4681d8
Maedeh <m.soroushmehr@gmail.com>
2022-03-06 16:44:35 +0330

    Publication is added to models

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Publication
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'publish')
+    list_display = ('title', 'image_tag', 'slug', 'publish')
     search_fields = ('title', 'description')
     prepulated_fields = {'slug':('title',)}
     ordering = ('status', 'publish')
 admin.site.register(Book, BookAdmin)
 # Register your models here.
+
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(Publication, PublicationAdmin)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,10 +2,17 @@ from turtle import update
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+from django.utils.html import format_html
 
 class User(AbstractUser):
     pass
 
+
+class Publication(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
 
 class Book(models.Model):
     STATUS_CHOICES = [('d', 'draft'), ('p', 'publish')]
@@ -17,5 +24,12 @@ class Book(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    publication = models.ForeignKey(Publication, null=True, on_delete=models.CASCADE)
+    def image_tag(self):
+        return format_html("<img width=100 height=75 src='{}'>".format(self.image.url))
     
-    
+    def __str__(self):
+        return self.title
+   
+   
+   
